f0444f11bf1fd5c4d5d16fd7b0af86be7e5a6b88
Tuandev18 <phamtuan180704@gmail.com>
2025-11-09 11:17:41 +0700

    authApi for login register forgotpassword  resetpassword changepassword

---
+++ src/api/authApi.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api";
+
+export const loginApi = async ({ email, password }) => {
+  const res = await axios.post(`${API_URL}/auth/login`, { email, password });
+  return res.data;
+};
+
+export const registerApi = async ({ username, email, password }) => {
+  const res = await axios.post(`${API_URL}/auth/register`, {
+    username,
+    email,
+    password,
+  });
+  return res.data;
+};
+
+export const getProfileApi = async (token) => {
+  const res = await axios.get(`${API_URL}/auth/me`, {
+    headers: { Authorization: `Bearer {token}` },
+  });
+  return res.datas;
+};
+
+export const updateProfileApi = async (
+  token,
+  { username, email, gender, avatar_url }
+) => {
+  const res = await axios.patch(
+    `${API_URL}/auth/me`,
+    { username, email, gender, avatar_url },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data;
+};
+
+export const forgotPasswordApi = async (email) => {
+  const res = await axios.post(`${API_URL}/auth/forgot-password`, { email });
+  return res.data;
+};
+
+export const resetPasswordApi = async (token, newPassword) => {
+  const res = await axios.post(`${API_URL}/auth/reset-password`, {
+    token,
+    newPassword,
+  });
+  return res.data;
+};
+
+export const changePasswordApi = async (token, { oldPassword, newPassword }) => {
+  const res = await axios.post(
+    `${API_URL}/auth/change-password`,
+    { oldPassword, newPassword },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  return res.data;
+};
