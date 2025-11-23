c290edf4e02aa4d457edd3f6672f20c5a2f96ee2
Tuandev18 <phamtuan180704@gmail.com>
2025-11-23 17:15:18 +0700

    api movie_api admin

---
+++ src/api/movie_apiadmin.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api", 
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`, 
+  },
+});
+
+
+export const getMovies = async (page = 1, limit = 5) => {
+  const response = await api.get("/admin/movies", {
+    params: { page, limit },
+  });
+  return response.data;
+};
+
+
+export const createMovie = async (movieData) => {
+  const response = await api.post("/admin/movies", movieData);
+  return response.data;
+};
+
+
+export const updateMovie = async (id, movieData) => {
+  const response = await api.put(`/admin/movies/${id}`, movieData);
+  return response.data;
+};
+
+
+export const deleteMovie = async (id) => {
+  const response = await api.delete(`/admin/movies/${id}`);
+  return response.data;
+};
