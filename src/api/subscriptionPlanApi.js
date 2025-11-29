2eac424b4a1b326c3f38c6aac63361c837e55137
Tuandev18 <phamtuan180704@gmail.com>
2025-11-29 22:24:20 +0700

    subcriptionPlanApi

---
+++ src/api/subscriptionPlanApi.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
+
+export const subscriptionPlanApi = {
+  getAll: async () => {
+    const res = await api.get("/admin/subscription-plans");
+    return res.data;
+  },
+  getById: async (id) => {
+    const res = await api.get(`/admin/subscription-plans/${id}`);
+    return res.data;
+  },
+  create: async (data) => {
+    const res = await api.post("/admin/subscription-plans", data);
+    return res.data;
+  },
+  update: async (id, data) => {
+    const res = await api.put(`/admin/subscription-plans/${id}`, data);
+    return res.data;
+  },
+  delete: async (id) => {
+    const res = await api.delete(`/admin/subscription-plans/${id}`);
+    return res.data;
+  },
+};
