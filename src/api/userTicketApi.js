86c2bbfb63292f0b4b387f00d89eae35aefe0f9d
Tuandev18 <phamtuan180704@gmail.com>
2025-11-30 11:40:25 +0700

    userTicketApi

---
+++ src/api/userTicketApi.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+export const userTicketApi = {
+  getAll: async () => {
+    const res = await api.get("/user/tickets");
+    return res.data;
+  },
+
+  getById: async (id) => {
+    const res = await api.get(`/user/tickets/${id}`);
+    return res.data;
+  },
+
+  book: async (data) => {
+    const res = await api.post("/user/tickets", data);
+    return res.data;
+  },
+
+  pay: async (id) => {
+    const res = await api.post(`/user/tickets/pay/${id}`);
+    return res.data;
+  },
+};
